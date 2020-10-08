10f250e3a51b61238adee1cc1075feefc17bd64e
Ronyellison Braga dos Santos <ronyjp@gmail.com>
2020-10-08 20:40:27 -0300

    Criaçao componente rodape

--- lab-angula/src/app/app.component.ts
+++ lab-angula/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lab-angula';
+  title = 'Projeto Angular';
+  nome = 'Ronyellison Braga';
 }
--- lab-angula/src/app/app.module.ts
+++ lab-angula/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
